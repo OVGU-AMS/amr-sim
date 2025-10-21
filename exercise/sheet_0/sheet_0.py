6fb010a7511959aa7c15333e922b2c3f81a60c99
Moritz Pötzsch <moripoe@icloud.com>
2025-10-21 12:33:30 +0200

    show robot position

--- exercise/sheet_0/sheet_0.py
+++ exercise/sheet_0/sheet_0.py
@@ -6,7 +6,7 @@ env = irsim.make("sheet_0.yaml", save_ani=False, full=False)
 for _i in range(1000):
     env.step()
     env.render(0.05)
-
+    env._env_plot.robot_pos = (env.get_robot_state()[0][0], env.get_robot_state()[1][0])
     if env.done():
         break
 
