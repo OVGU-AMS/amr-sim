506637769d8a7cf0cd49f9f830d2a56480261ac7
Moritz Pötzsch <moritz.poetzsch@ovgu.de>
2025-12-09 11:38:06 +0100

    New Sheet 7

--- exercise/sheet_7/sheet_7.py
+++ exercise/sheet_7/sheet_7.py
@@ -5,12 +5,34 @@ from typing import List
 
 
 '''
-EXERCISE SHEET 6
+EXERCISE SHEET 7
 
 In this exercise a position estimation is calculated, based on the LiDAR measurements of the robot.
 Additionally, the identity of the hit wall is known, as well as its geometrical properties.
 Using this a virtual measurement is constructed, which is then used to calculate the position of the robot.
+
+This time the estimate is calculated using recursive least squares.
 '''
+
+def transform_to(x: np.ndarray, t: np.ndarray, a: float):
+    '''Transforms a pose or velocity into an other coordinate frame by translating and rotating.
+    (This function was already implemented in sheet 1)
+
+    Args:
+        x (np.ndarray): State or velocity which should be transformed to a new coordinate frame.
+        t (np.ndarray): Translation of the original coordinate frame in the target frame.
+        a (float): Angle of the original coordinate frame in the target frame.
+
+    Returns:
+        np.ndarray: Pose or velocity in the new coordinate frame.
+    '''
+    
+    R = np.array([[math.cos(a), - math.sin(a)],
+                  [math.sin(a), math.cos(a)]])
+    
+    x_transformed = t + R @ x
+    
+    return x_transformed
 
 def build_H(normals):
     """
@@ -23,9 +45,7 @@ def build_H(normals):
         np.ndarray: Measurement mapping matrix H
     """
 
-    # TODO build the H matrix using the normals
-    
-    H = np.array([[0.0,0.0],[0.0,0.0]])
+    H = np.stack(normals)
     return H
 
 def build_y(normals, wall_dists, deltas, lidar_ranges):
@@ -42,42 +62,67 @@ def build_y(normals, wall_dists, deltas, lidar_ranges):
     Returns:
         np.ndarray: Virtual measurements
     """
+    
+    s = np.cos(deltas) * lidar_ranges
+    y = wall_dists + s
+    return y
 
-    # TODO calculate the virtual measurements y. Use your derived transformation to orthogonal measurements.
-
-    y = [0.0,0.0]
-
-    return np.array(y)
-
-def position_from_block_lsq(normals, wall_dists, deltas, lidar_ranges, lidar_pos, heading):
-    """
-    Calculates the position of the robot using lidar measurements to walls.
+def build_gain(previous_C, H, W):
+    """Calculates the gain matrix K
 
     Args:
-        normals (np.ndarray): Normal vectors of the hit walls.
-        wall_dists (np.ndarray): Distances of the hit walls to the coordinate origin. 
-            Together with `normals` they describe the walls as lines using the hessian form.
-        deltas (np.ndarray): Angle between LiDAR ray and normal vector of the wall.
-        lidar_ranges (np.ndarray): Measured ranges of the LiDAR rays.
-        lidar_pos (np.ndarray): Position of the LiDAR sensor in the robot coordinate frame.
-        heading (float): Heading of the robot.
+        previous_C (np.ndarray): Covariance of the estimate of the previous time step.
+        H (np.ndarray): Measuremten mapping matrix.
+        W (np.ndarray): Weighting matirx / Error of the current measurement.
+
+    Returns:
+        np.ndarray: Gain matrix K
+    """
+    
+    # TODO create the gain matrix K using the equation from the lecture
+    K = 
+    
+    return K
+
+def position_from_recursive_lsq(previous_estimate, previous_covariance, normal, wall_dist, delta, lidar_range):
+    """
+    Calculates a new position estimation of the robot using one lidar measurement to a wall and the previous estimation.
+
+    Args:
+        previous_estimate (np.ndarray): Position estimate as `(x,y)` calculated in the previous time step.
+        previous_covariance (np.ndarray): Covariance of the estimate of the previous time step.
+        normal (np.ndarray): Normal vector of the hit wall.
+        wall_dist (np.ndarray): Distance of the hit wall to the coordinate origin. 
+            Together with `normal` they describe the wall as line using the hessian form.
+        delta (np.ndarray): Angle between the normal vector of the wall and the LiDAR ray hitting the wall
+        lidar_range (np.ndarray): Measured ranges of the LiDAR rays.
 
     Returns:
         np.ndarray: Position of the robot as `(x,y)`
     """
+
+    # TODO build the weighting / error matrix (HINT the standard deviation of the lidar is 0.2) 
+    W = 
     
-    # TODO Use the lidar rays, the known information about the walls and your implemented functions to calculate the position od the lidar.
-    #      Then transform this to the robots position.
+    # TODO fill the H and y by passing the correct values in the respecting functions
+    H = 
+    y = 
     
-    p = np.array([0.0,0.0])
+    # TODO fill K by passing the correct values in the respecting function
+    K = 
     
-    return p
+    # TODO use the equations from the lecture to compute x and C
+    x = 
+
+    C = 
+    
+    return x, C
 
 
 # ---- Not relevant for you ----
 
 
-env = irsim.make("sheet_6.yaml", save_ani=False, full=False)
+env = irsim.make("sheet_7.yaml", save_ani=False, full=False)
 
 lines = [[1/25, 0, 'blue'],
          [25, 0, 'red'],
@@ -131,7 +176,10 @@ for _i in range(1000):
         wall_dists.append(c)
         deltas.append(delta)
         lidar_ranges.append(lidar_measurements[i][0])
-    estimation = position_from_block_lsq(normals, wall_dists,deltas,lidar_ranges, lidar_pose, robot_position[2])
+    estimation = np.array([0,0])
+    covariance = np.array([[1000,0],[0,1000]])
+    for normal, wall_dist, delta, lidar_range in zip (normals, wall_dists,deltas,lidar_ranges):
+        estimation, covariance = position_from_recursive_lsq(estimation, covariance, np.array([normal]), np.array([wall_dist]),np.array([delta]),np.array([lidar_range]))
     env._env_plot.estimation_pos = estimation
 
     if env.done():
